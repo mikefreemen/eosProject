65eaeb80e324ad4dfe9113def9f7b3329c8f6fa4
Mike Freemen <mike@mikefreemen.com>
2018-08-28 23:52:40 -0300

    expand/collapse view works (and is hideous)

--- eos-interaction/src/SingleBlock.js
+++ eos-interaction/src/SingleBlock.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 
 class SingleBlock extends Component {
+  state = {
+    detailedView: false
+  }
+
   logApiCalls(respPromise) {
     respPromise.then(resp => {
       console.log(resp)
@@ -8,17 +12,31 @@ class SingleBlock extends Component {
       console.log('Error...')
       console.log(err)
     })
-
   }
 
-  render() {
-    console.log('from SingleBlock:')
-    console.log(this.props.blockInfo)
-    return this.props.blockInfo && (<div>
+  handleBlockClick = () => {
+    this.setState((prevState) => {
+      return {detailedView: !prevState.detailedView}
+    })
+  }
+
+  blockSummary = (props) => (
+    <div onClick={this.handleBlockClick}>
       <div>Block hash: {this.props.block_num}</div>
       <div>Timestamp: {this.props.blockInfo.timestamp}</div>
       <div># Actions in block: {this.props.blockInfo.transactions.length}</div>
-      {/*<div>[{JSON.stringify(this.props.blockInfo, null, 2)}]</div>*/}
+    </div>
+  )
+
+  blockDetails = (props) => (
+    <div onClick={this.handleBlockClick}>{JSON.stringify(this.props.blockInfo, null, 2)}</div>
+  )
+
+  render() {
+    if( !this.props.blockInfo ) return null
+    return (<div>
+      { this.state.detailedView && this.blockDetails() }
+      { !this.state.detailedView && this.blockSummary()}
     </div>)
   }
 }
